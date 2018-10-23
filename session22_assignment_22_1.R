9af4ac570a3cbb1905ff39b86339160d5bccb3f0
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-10-23 22:11:38 +0530

    Update session22_assignment_22_1.R

--- session22_assignment_22_1.R
+++ session22_assignment_22_1.R
@@ -8,71 +8,100 @@ epi_r<-read.csv("C:/Users/DELL/Documents/epi_r.csv")
 View(epi_r)
 sum(is.na(epi_r))
 
-###################################
-epi_r<-epi_r[,1:6]
-kmeans(epi_r,5)
-View(epi_r)
+library(tidyverse)
 
-complete.cases(epi_r)
-nrow(epi_r[!complete.cases(epi_r),])
-library(mice)
-median(epi_r$calories, na.rm = TRUE)
-mean(epi_r$calories, na.rm = TRUE)
+epi_r <- epi_r %>%
+  filter(calories < 8000) %>% 
+  filter(protein<200) %>%
+  filter(fat < 600) %>% 
+  filter(sodium<1500) %>% # remove outliers
+  na.omit() # remove rows with NA values
 
-median(epi_r$protein, na.rm = TRUE)
-mean(epi_r$protein, na.rm = TRUE)
-
-
-
-
-
-
-epi_rr<-epi_r
-View(epi_rr)
-library(VIM)
-aggr_plot <- aggr(epi_rr, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(epi_rr), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
-marginplot(epi_rr[c(1,2)])
-
-
-
-library(mice)
-md.pattern(epi_rr)
-epi_rr <- mice(epi_rr,m=5,maxit=10,meth='pmm',seed=500)
-summary(epi_rr)
-
-library(lattice)
-densityplot(epi_rr)
-
-epi_rr$imp$protein
-
-completed_epi<-complete(epi_rr,1)
-View(completed_epi)
-############################################
 
 
 #a.Apply k means clustering to identify similar recepies
-epi_r<-epi_r[,-c(1:6)]
-epi_r<-scale(epi_r)
-summary(epi_r)
-epi_rr<-kmeans(epi_r,1)
-epi_rr$withinss
-epi_rr$tot.withinss
 
-epi_rr<-kmeans(epi_r,2)
-epi_rr$withinss
-epi_rr$tot.withinss
+rec<-epi_r[, 1:6]
+set.seed(12345)
+reci<-kmeans(rec[,c("calories","protein","fat","sodium")], centers = 10)
+reci
+o<-order(reci$cluster)
+head(data.frame(rec$title[o],reci$cluster[o]))
 
-epi_rr<-kmeans(epi_r,4)
-epi_rr$iter
-epi_rr$tot.withinss
-summary(epi_rr)
 
-epi_rr$centers
+library(cluster)
+clusplot(rec,reci$cluster, main = "2d representation of recipe clustering", color=TRUE, shade = TRUE, labels = 2, lines = 0)
+
+d<-dist(rec[,-1], method = "euclidean")
+d
+h.fit<-hclust(d, method = "ward.D2")
+
+plot(h.fit)
+
+groups<-cutree(h.fit, k=7)
+rect.hclust(h.fit, k=7, border = "red")
+
+table(rec[,1], groups)
+
+recipes<-agnes(rec, metric = "euclidian", method = 'ward.D2')
+
 
 #b.Apply k means clustering to identify similar Attributes
 
+
+y<-attributes(rec)
+y
+x<-rec[, c(2:6)]
+
+View(x)
+epi_rrr<-scale(x)
+df1<-kmeans(x,7)
+
+c<-attributes(df1)
+df1$centers
+df1$cluster
+df1$withinss
+df1$tot.withinss
+df1$size
+
+
+
+df2<-kmeans(x,8)
+df2$cluster
+
+df2$withinss
+df2$tot.withinss
+
+df2$size
+
+
+df3<-kmeans(x,9)
+df3$cluster
+
+df3$withinss
+df3$tot.withinss
+
+df3$size
+
 #c.how many unique recepies that people order often
+
+library(dplyr)
+
+library(data.table)
+t<-unique(rec,incomparables = FALSE)
+
 
 #d. what are their typical profiles
 
+library(dplyr)
+f<-distinct(rec,.keep_all = FALSE)
+
+
+tx<-describe(rec)
+
+par(mfrow=c(2,4))
+boxplot(rec$sodium)
+boxplot(rec$calories)
+boxplot(rec$protein)
+boxplot(rec$fat)
 
